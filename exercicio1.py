b8489dbd01ebf1194b902a702e433cfb0d25c197
Dev. Lucas Galindo <146115927+Lucras22@users.noreply.github.com>
2024-08-18 13:08:44 -0300

    Create exercicio1.py

---
+++ exercicio1.py
@@ -0,0 +1,24 @@
+number = input("Digite um número: ")
+
+if number == '0':
+    print("Escreveu 0")
+elif number == '1':
+    print("Escreveu 1")
+elif number == '2':
+    print("Escreveu 2")
+elif number == '3':
+    print("Escreveu 3")
+elif number == '4':
+    print("Escreveu 4")
+elif number == '5':
+    print("Escreveu 5")
+elif number == '6':
+    print("Escreveu 6")
+elif number == '7':
+    print("Escreveu 7")
+elif number == '8':
+    print("Escreveu 8")
+elif number == '9':
+    print("Escreveu 9")
+else:
+    print("Numero invalido!")
