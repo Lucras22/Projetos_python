4feea79957862e2cf760e735a6ff237c887be837
Dev. Lucas Galindo <146115927+Lucras22@users.noreply.github.com>
2024-08-18 13:09:44 -0300

    Create loja.py

---
+++ loja.py
@@ -0,0 +1,14 @@
+print("Bem Vindo")
+
+valor = 100.00
+custo = 40.00
+desconto = 0.15
+lucro = valor - custo
+
+print ("O valor do prodturo é: ", valor ,"R$")
+
+print("Com desconto a sua compra fica: ", valor * desconto ,"R$")
+
+print("O vendedor gastou: ", custo ,"R$ por esse produto")
+
+print("O vendedor lucrou: ", lucro , "R$ com a sua compra")
